c5bce57c6a196d99c878e47886400905b78fc6b8
Duy Truong <truongbaoduy2003@gmail.com>
2025-01-13 11:22:38 +0700

    Add POST request for Admin

--- app/api/accounts/route.js
+++ app/api/accounts/route.js
@@ -5,6 +5,7 @@ import { AppraisalBoard } from "@/models/users/AppraisalBoard";
 import { TechnologyScience } from "@/models/users/TechnologyScience";
 import { NextResponse } from "next/server";
 import { Instructor } from "@/models/users/Instructor";
+import bcrypt from "bcrypt";
 
 export async function GET(request) {
   try {
@@ -24,6 +25,39 @@ export async function GET(request) {
     return NextResponse.json(accounts, { status: 200 });
   } catch (error) {
     return new NextResponse("Lỗi lấy tài khoản " + error.message, {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(request) {
+  try {
+    const { name, email, phone, password, role } = await request.json();
+
+    await mongooseConnect();
+
+    if (await Account.findOne({ email })) {
+      return NextResponse.json(
+        { message: "Email đã được sử dụng!" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await Account.create({
+      name,
+      email,
+      phone,
+      password: hashedPassword,
+      role,
+    });
+
+    return NextResponse.json(
+      { message: "Tài khoản quản trị viên đã được tạo thành công!" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return new NextResponse("Lỗi tạo tài khoản admin " + error, {
       status: 500,
     });
   }
