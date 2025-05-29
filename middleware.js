f4945df035c86e16a6d612ebf19308539c330549
Duy Truong <truongbaoduy2003@gmail.com>
2025-05-29 14:56:47 +0700

    Switching from getToken to auth

--- middleware.js
+++ middleware.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
 import { authConfig } from "@/auth.config";
 import {
   DEFAULT_LOGIN_REDIRECT,
@@ -22,49 +21,36 @@ const ROLE_DEFAULT_PATH = {
 export default auth(async (req) => {
   const { nextUrl: url } = req;
   const origin = url.origin;
-  console.log(origin);
 
-  // 1) Let NextAuth handle its own API‐auth routes
   if (url.pathname.startsWith(apiAuthPrefix)) {
     return NextResponse.next();
   }
 
-  // 2) Fetch token + derive login state & role
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  const isLoggedIn = !!token;
-  const userRole = token?.role;
-  console.log(userRole);
-  console.log(token);
+  const session = await auth();
+  const isLoggedIn = !!session;
+  const userRole = session?.user?.role;
 
-  // 3) If visiting a NEXT‐AUTH page…
   if (authRoutes.includes(url.pathname)) {
-    // → already logged in? send to dashboard
     if (isLoggedIn) {
       const dest = ROLE_DEFAULT_PATH[userRole] ?? DEFAULT_LOGIN_REDIRECT;
       return NextResponse.redirect(new URL(dest, origin));
     }
 
-    // → else let them see login/register
     return NextResponse.next();
   }
 
-  // 4) If not logged in AND not a public page, force login
   if (!isLoggedIn && !publicRoutes.includes(url.pathname)) {
     return NextResponse.redirect(new URL("/auth/login", origin));
   }
 
-  // 5) If logged in AND has a role, ensure they're inside their role‐area
   if (isLoggedIn && userRole) {
-    // If at root path, redirect to role-specific path
     if (url.pathname === "/") {
       const dest = ROLE_DEFAULT_PATH[userRole] ?? DEFAULT_LOGIN_REDIRECT;
       return NextResponse.redirect(new URL(dest, origin));
     }
 
     const base = `/${userRole}`;
-    // If they're outside of `/admin`, `/student`, etc.…
     if (!url.pathname.startsWith(base)) {
-      // → send them to that role's default path
       const dest = ROLE_DEFAULT_PATH[userRole] ?? DEFAULT_LOGIN_REDIRECT;
       return NextResponse.redirect(new URL(dest, origin));
     }
@@ -73,7 +59,6 @@ export default auth(async (req) => {
   return NextResponse.next();
 });
 
-// Tất cả url match với config thì middleware sẽ chạy
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
